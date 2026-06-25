10be09c51095254c7dc09f8fedc14e0a94ede13c
tan802 <tanisha.debnath@cloudkaptan.com>
2026-06-25 15:49:11 +0000

    datatypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,29 @@
+"use strict";//treat all JS code a newer version 
+
+//alerts("hello")
+//alert(3+3) //we are using nodejs not browser
+
+console.log(3 + 3) // code readability
+console.log("Hitesh")
+
+let name="Tanisha"//string 
+let age = 18 // variable
+let isLoggedIn = false // bolean
+let state;//undefined
+//let state = null; //null
+
+//number => 2 to the power 53
+//bigInt for tsock and trading website
+//string => ""
+//boolean => true/false
+//null => standalone value and is an object
+//undefined => no value assigned
+// symbol => unique
+
+
+console.log(typeof "Tanisha");
+console.log(typeof age);
+
+console.log(typeof null);//ogject
+
+console.log(typeof undefined);//undefined
---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail ="tanisha@google.com"
+var accountPassword ="12345"
+accountCity = "Jaipur"
+let accountState;
+//accountId = 2
+
+/* var is not used due to the issue of block and functional scope*/
+accountEmail = "tan.com"
+accountPassword = "2121"
+accountCity = "Bengaluru" 
+console.log(accountId );
+console.table([accountId, accountEmail ,accountPassword,accountCity , accountState ])
